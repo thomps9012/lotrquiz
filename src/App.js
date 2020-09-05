c36a7d2d623b652d853811b7badc0fa5a496639c
Samuel Thompson <thomps9012@gmail.com>
2020-09-05 14:28:01 -0400

    creating multiple components, reached natural breaking point

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Question from './components/Question';
+import { render } from '@testing-library/react';
+import quizQuestions from './api/quizQuestions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      counter: 0,
+      questionId: 1,
+      question: '',
+      answerOptions: [],
+      answer: '',
+      answersCount: {},
+      result: ''
+    };
+    this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
+  }
+  componentDidMount() {
+    const shuffledAnswerOptions = quizQuestions.map((question) => this.shuffleArray(question.answers));
+
+    this.setState({
+      question: quizQuestions[0].question,
+      answerOptions: shuffledAnswerOptions[0]
+    });
+  }
+  shuffleArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+  // might need to come back here and update/change to code for correct answer
+  setUserAnswer(answer) {
+    this.setState((state) => ({
+      answersCount: {
+        ...state.answersCount,
+        [answer]: (state.answersCount[answer] || 0) + 1
+      },
+      answer: answer
+    }));
+  }
+  handleAnswerSelected(event) {
+    this.setUserAnswer(event.currentTarget.value);
+    if (this.state.questionId < quizQuestions.length) {
+        setTimeout(() => this.setNextQuestion(), 300);
+      } else {
+
+      }
+  }
+  render (){
+    return(
+      <div className="App">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>React Quiz</h2>
+      </div>
+      <Quiz
+        answer={this.state.answer}
+        answerOptions={this.state.answerOptions}
+        questionId={this.state.questionId}
+        question={this.state.question}
+        questionTotal={quizQuestions.length}
+        onAnswerSelected={this.handleAnswerSelected}
+      />
     </div>
-  );
+    )
 }
-
-export default App;
+}
---
+++ src/api/quizQuestions.js
@@ -0,0 +1,67 @@
+var quizQuestions = [
+    { question: "In 'The Fellowship of the Ring', what is the name of the ferry the hobbits use to escape the Black Riders?",
+        answers: [
+            "Bucklebury Ferry",
+            "Midnight Rider",
+            "Our Lady o' the River"
+        ]
+    },
+    { question: "Where was the entire trilogy of the Lord of the Rings filmed?",
+        answers: [
+            "New Zealand",
+            "Australia",
+            "Greenland"
+        ]
+    },
+    { question: "What mythical creatures are constantly hunting Frodo and his merry crew?",
+        answers: [
+            "Nazgul",
+            "Bombadil",
+            "Rohirrim"
+        ]
+    },
+    { question: "What book is the prequel to the LoTR series?",
+        answers: [
+            "Silmarillion",
+            "Two Towers",
+            "Prisoner of Azkaban"
+        ]
+    },
+    { question: "What is the name of the character who returns from the dead and rides into battle at Helms Deep?",
+        answers: [
+            "Gandalf the Grey",
+            "Gandalf the White",
+            "Saruman"
+        ]
+    },
+    { question: "In Mordor, what kind of food does Frodo say he can't remember the taste of?",
+        answers: [
+            "Strawberries",
+            "Potatoes",
+            "Raspberries"
+        ]
+    },
+    { question: "Where does the Fellowship of the Ring break up?",
+        answers: [
+            "Amon Hen",
+            "Anduin",
+            "Gondor"
+        ]
+    },
+    { question: "Who became King of Rohan after Theoden met his demise in battle?",
+        answers: [
+            "Eomer",
+            "Boromir",
+            "Aragon"
+        ]
+    },
+    { question: "How many wizards are there in Middle Earth?",
+        answers: [
+            "5",
+            "3",
+            "7"
+        ]
+    }
+  ];
+  
+  export default quizQuestions;
