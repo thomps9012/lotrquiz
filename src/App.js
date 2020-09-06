67ca0e961d418fb0c5445d6726a353c6c36cd7b6
Samuel Thompson <thomps9012@gmail.com>
2020-09-06 14:13:26 -0400

    cleaned up App imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Question from './components/Question';
 import Quiz from './components/Quiz';
-import AnswerOption from './components/AnswerOption';
-import { render } from '@testing-library/react';
 import quizQuestions from './api/quizQuestions';
 import Result from './components/Result';
 
