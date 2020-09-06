1eaa5cab9a8be84814850a9b68c78c1101349a41
Samuel Thompson <thomps9012@gmail.com>
2020-09-06 14:30:14 -0400

    updated favicon to lotr logo

--- src/App.js
+++ src/App.js
@@ -108,8 +108,8 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>LoTR React Quiz</h2>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2>LoTR Character Quiz</h2>
         </div>
         {this.state.result ? this.renderResult() : this.renderQuiz()}
       </div>
